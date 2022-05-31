dffe51e8d7277eea4c7f9f9b51993067e27d47fa
Luis Corbi <luis-corbi@outlook.com>
2022-05-31 11:49:21 -0300

    subida a heroku

--- src/public/server.js
+++ src/public/server.js
@@ -30,12 +30,7 @@ if (modoCluster && cluster.isPrimary) {
     const app = express()
     app.use(express.static('public'))
     app.get('/', (req, res) => {
-        const primes = []
-        const max = Number(req.query.max) || 1000
-        for (let i = 1; i <= max; i++) {
-            if (isPrime(i)) primes.push(i)
-        }
-        res.json(primes)
+        res.send('prueba subida heroku');
     })
  
     app.listen(PORT, () => {
@@ -44,19 +39,6 @@ if (modoCluster && cluster.isPrimary) {
     })
  }
  
- function isPrime(num) {
-    if ([2, 3].includes(num)) return true;
-    else if ([2, 3].some(n => num % n == 0)) return false;
-    else {
-        let i = 5, w = 2;
-        while ((i ** 2) <= num) {
-            if (num % i == 0) return false
-            i += w
-            w = 6 - w
-        }
-    }
-    return true
- } 
 
  app.get("/getUsers", (req, res) => {
     res.json({ users })
