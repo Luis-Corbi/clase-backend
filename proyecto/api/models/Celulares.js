cc6200d5e4aee0a593b675deff9e7bfacea97bc8
Luis Corbi <luis-corbi@outlook.com>
2022-07-12 23:03:56 -0300

    desafio clase 46

---
+++ proyecto/api/models/Celulares.js
@@ -0,0 +1,34 @@
+/**
+ * Celulares.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    nombre: { type: 'string', required: true },
+    precio: { type: 'number', required: true },
+    descripcion: { type: 'string', required: true },
+    marca: { type: 'string', required: true },
+    imagen: { type: 'string'},
+    categoria: { type: 'string'},
+  },
+
+};
+
